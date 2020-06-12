23ecf48d40b57b6002cae0369978b8c92aece7b7
Jake Malis <jake@allstaresports.com>
2020-06-11 22:08:15 -0400

    Finished login

--- public/authentication.js
+++ public/authentication.js
@@ -33,30 +33,6 @@ function passwordSignIn() {
         }
     }
 }
-function passwordSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (validateEmail(email) === true) {
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(
-            function(error) {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                if (errorCode == 'auth/weak-password') {
-                    alert('Password too weak')
-                }
-                else {
-                    alert(errorMessage);
-                }
-            });
-        firebase.auth().currentUser.sendEmailVerification().then(
-            function(){
-                alert('Verification Email Sent');
-            });
-    }
-    else if (validateEmail(email) === false) {
-        alert('Email address is not valid');
-    }
-}
 function passwordReset() {
     var email = document.getElementById('email').value;
     firebase.auth().sendPasswordResetEmail(email).then(
@@ -83,17 +59,17 @@ function initApp() {
             var isAnonymous = user.isAnonymous;
             var uid = user.uid;
             var providerData = user.providerData;
-            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
             document.getElementById('sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
             document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
         } else {
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
             document.getElementById('quickstart-account-details').textContent = 'null';
         }
     });
-    document.getElementById('signIn').addEventListener('click', passwordSignIn, false);
+    document.getElementById('sign-in').addEventListener('click', passwordSignIn, false);
     document.getElementById('register').addEventListener('click', passwordSignUp, false);
-    document.getElementById('passwordReset').addEventListener('click', passwordReset, false);
+    document.getElementById('password-reset').addEventListener('click', passwordReset, false);
 }
 window.onload = function() {
     initApp();
---
+++ public/registration.js
@@ -0,0 +1,98 @@
+function validateEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+    {
+        return (true)
+    }
+    return (false)
+}
+
+function register() {
+    var fortnite = false;
+    var overwatch = false;
+    var smash = false;
+    var valorant = false;
+
+    if (document.getElementById('player').checked) {
+        var role = "player";
+        var fortnite = document.getElementById("fortnite").checked;
+        var overwatch = document.getElementById("overwatch").checked;
+        var smash = document.getElementById("smash").checked;
+        var valorant = document.getElementById("valorant").checked;
+    }
+    else if (document.getElementById('coach').checked) {
+        var role = "coach";
+    }
+
+    //Change HTML Title
+    //document.getElementById('title').innerHTML = fortnite + " " + overwatch;
+
+    if (validateEmail(document.getElementById('email').value) === true) {
+        firebase.auth().createUserWithEmailAndPassword(document.getElementById('email').value, document.getElementById('password').value).catch(
+            function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode == 'auth/weak-password') {
+                    alert('Password too weak')
+                }
+                else {
+                    alert(errorMessage);
+                }
+            });
+        setTimeout(function(){
+            verifyEmail();
+            db.collection("users").doc(firebase.auth().currentUser.uid).set({
+                first: document.getElementById('first-name').value,
+                last: document.getElementById('last-name').value,
+                email: document.getElementById('email').value,
+                state: document.getElementById('state').value,
+                highschool: document.getElementById('highschool').value,
+                role: role,
+                games: {
+                    fortnite: fortnite,
+                    overwatch: overwatch,
+                    smash: smash,
+                    valorant: valorant
+                }
+            }).then(function() {
+                console.log("Document successfully written!");
+            })
+                .catch(function(error) {
+                    console.error("Error writing document: ", error);
+                });
+        }, 5000);
+    }
+    else if (validateEmail(document.getElementById('email').value) === false) {
+        alert('Email address is not valid');
+    }
+}
+
+function verifyEmail() {
+    firebase.auth().currentUser.sendEmailVerification().then(
+        function(){
+            alert('Verification Email Sent');
+        });
+}
+
+function initApp() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+        } else {
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+        }
+    });
+    document.getElementById('register').addEventListener('click', register, false);
+}
+
+window.onload = function() {
+    initApp();
+}
