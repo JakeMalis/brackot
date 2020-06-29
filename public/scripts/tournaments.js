ffd95b1fa82ed6a328131ee8ccdfcfd4693d2628
Jake Malis <jake@allstaresports.com>
2020-06-29 02:58:35 -0400

    Started massive bug fixes on tournaments page

--- public/scripts/tournaments.js
+++ public/scripts/tournaments.js
@@ -2,7 +2,7 @@ const tournaments = new Array();
 
 window.onload = function() {
   firebase.auth().onAuthStateChanged(function(user) {
-      if (user) { loadData(); }
+      if (user) { document.getElementById("avatar").src = firebase.auth().currentUser.photoURL; loadTournaments(); }
       else { window.location = "login.html"; }
   });
 }
@@ -19,46 +19,57 @@ function enroll(tournamentNumber) {
   });
 }
 
-function loadData() {
+function loadTournaments() {
   firebase.firestore().collection("tournaments").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        //console.log(doc.id, " => ", doc.data());
+        console.log(doc.id, " => ", doc.data());
         tournaments.push(doc.data());
     });
   }).then(function() {
-      loadCards();
-  }).then(function() {
       personalizeElements();
   });
-}
-
-function loadCards() {
-  for (index = 0; index < tournaments.length; index++) {
-    document.getElementById("tournamentCard" + (index + 1)).style.visibility = "visible";
-  }
 }
 
 function personalizeElements() {
   for (index = 0; index < tournaments.length; index++) {
     const tournament = tournaments[index];
     var tournamentId = (tournament.date + "-" + tournament.game);
+    var cardNumber = index + 1;
+
+    document.getElementById("tournamentCard" + cardNumber).style.visibility = "visible";
+
+    document.getElementById("tournamentWallpaper" + cardNumber).src = "/media/game_wallpapers/" + tournament.game + "-" + "gameplay.jpg";
+    document.getElementById("tournamentTitle" + cardNumber).innerHTML = tournament.name;
+    document.getElementById("tournamentGame" + cardNumber).innerHTML = tournament.game;
+    document.getElementById("tournamentDate" + cardNumber).innerHTML = tournament.elegant_date;
 
     var tournamentDocument = firebase.firestore().collection("tournaments").doc(tournamentId);
-    tournamentDocument.collection("players").get().then(function(doc) {
-      document.getElementById("tournamentWallpaper" + index).src = "/media/game_wallpapers/" + "fortnite" + "gameplay.png";
-      document.getElementById("tournamentTitle" + index).innerHTML = tournament.name;
-      document.getElementById("tournamentGame" + index).innerHTML = tournament.game;
-      document.getElementById("tournamentDate" + index).innerHTML = tournament.elegant_date;
-      document.getElementById("tournamentParticipants" + index).innerHTML = doc.size + " Participants";
-    });
     tournamentDocument.collection("players").doc(firebase.auth().currentUser.uid).get().then(function(doc) {
       if (doc.exists) {
-        document.getElementById("tournamentCardButton" + index).classList.toggle('tournamentCardButtonSigned');
-        document.getElementById("tournamentCardButton" + index).innerHTML = "✓ Signed Up";
-        document.getElementById("tournamentCardButton" + index).disabled = true;
+        document.getElementById("tournamentCardButton" + cardNumber).classList.toggle('tournamentCardButtonSigned');
+        document.getElementById("tournamentCardButton" + cardNumber).innerHTML = "✓ Signed Up";
+        document.getElementById("tournamentCardButton" + cardNumber).disabled = true;
       }
     });
   }
+}
 
-  document.getElementById("avatar").src = firebase.auth().currentUser.photoURL;
+function loadData() {
+  for (index = 0; index < tournaments.length; index++) {
+    const tournament = tournaments[index];
+    var tournamentId = (tournament.date + "-" + tournament.game);
+    var cardNumber = index + 1;
+
+    var tournamentDocument = firebase.firestore().collection("tournaments").doc(tournamentId);
+    tournamentDocument.collection("players").get().then(function(doc) {
+      document.getElementById("tournamentParticipants" + cardNumber).innerHTML = doc.size + " Participants";
+    });
+    tournamentDocument.collection("players").doc(firebase.auth().currentUser.uid).get().then(function(doc) {
+      if (doc.exists) {
+        document.getElementById("tournamentCardButton" + cardNumber).classList.toggle('tournamentCardButtonSigned');
+        document.getElementById("tournamentCardButton" + cardNumber).innerHTML = "✓ Signed Up";
+        document.getElementById("tournamentCardButton" + cardNumber).disabled = true;
+      }
+    });
+  }
 }
