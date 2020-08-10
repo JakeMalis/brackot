f9a81905d5c38542fd39a76fbe16f316d8849bfb
Jake Malis <jake@allstaresports.com>
2020-08-09 23:23:30 -0400

    Additional boost funcitonality

--- public/scripts/boosts.js
+++ public/scripts/boosts.js
@@ -1,8 +1,11 @@
 function personalizeElements() {
   $('#allStarUnlimited').click(function(){
-    startCheckout();
+    startCheckout('price_1H9lSLCwIH03I9fOjH0MgLEh');
   });
   $('#coinBoost').click(function(){
+    startCheckout('price_1H9lRPCwIH03I9fOb51Xqvyo');
+  });
+  $('#billingInfoButton').click(function(){
     openPortal();
   });
 }
@@ -13,24 +16,24 @@ async function openPortal() {
   window.location.assign(data.url);
 }
 
-async function startCheckout() {
+async function startCheckout(product) {
   const docRef = await firebase.firestore()
   .collection('customers')
   .doc(firebase.auth().currentUser.uid)
   .collection('checkout_sessions')
   .add({
-    price: 'price_1H9lSLCwIH03I9fOjH0MgLEh',
+    price: product,
     success_url: window.location.origin,
     cancel_url: window.location.origin,
   });
-// Wait for the CheckoutSession to get attached by the extension
-docRef.onSnapshot((snap) => {
-  const { sessionId } = snap.data();
-  if (sessionId) {
-    // We have a session, let's redirect to Checkout
-    // Init Stripe
-    const stripe = Stripe('pk_test_Q5Jva7sim6yZzEDARaqK8j5m00BSs2z9Ww');
-    stripe.redirectToCheckout({ sessionId });
-  }
-});
+  // Wait for the CheckoutSession to get attached by the extension
+  docRef.onSnapshot((snap) => {
+    const { sessionId } = snap.data();
+    if (sessionId) {
+      // We have a session, let's redirect to Checkout
+      // Init Stripe
+      const stripe = Stripe('pk_test_Q5Jva7sim6yZzEDARaqK8j5m00BSs2z9Ww');
+      stripe.redirectToCheckout({ sessionId });
+    }
+  });
 }
