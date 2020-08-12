7fe593e28b3e45f3b5e8888052075f51df6f0dd1
Jake Malis <jake@allstaresports.com>
2020-08-12 01:42:04 -0400

    Changed date and time for tournament creation on the backend

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,17 +1,34 @@
 window.onload = function() {
   firebase.auth().onAuthStateChanged(function(user) {
-      if (user) { document.getElementById("avatar").src = firebase.auth().currentUser.photoURL; }
+      if (user) { document.getElementById("avatar").src = firebase.auth().currentUser.photoURL; personalizeElements(); }
       else { window.location = "login.html"; }
   });
 }
 
+function personalizeElements() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  if(dd<10){
+    dd='0'+dd
+  }
+  if(mm<10){
+    mm='0'+mm
+  }
+
+  today = yyyy+'-'+mm+'-'+dd;
+  document.getElementById("date").setAttribute("min", today);
+}
+
 function addTournament() {
-  firebase.firestore().collection("tournaments").doc(document.getElementById('dateSimple').value + "-" + document.getElementById('game').value).set({
-      date: document.getElementById('dateSimple').value,
-      elegant_date: document.getElementById('dateElegant').value,
+  var date = document.getElementById("date").value, time = document.getElementById("time").value;
+
+  firebase.firestore().collection("tournaments").add({
+      creator: firebase.auth().currentUser.uid,
+      date: new Date(date + " " + time),
       game: document.getElementById('game').value,
       name: document.getElementById('tournamentName').value,
-      time: document.getElementById('time').value,
       players: [],
       earnings: {
           1: parseInt(document.getElementById('firstEarnings').value),
