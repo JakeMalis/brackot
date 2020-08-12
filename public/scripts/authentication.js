27e305e1bb74919b7d7603b0bfab17ad7bc118d0
Jake Malis <jake@allstaresports.com>
2020-08-12 00:08:19 -0400

    Added 'Remember me' functionality to login

--- public/scripts/authentication.js
+++ public/scripts/authentication.js
@@ -1,4 +1,6 @@
 function passwordSignIn() {
+  if (document.getElementById("rememberMe").checked) {
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(function() {
       firebase.auth().signInWithEmailAndPassword(document.getElementById('email').value, document.getElementById('password').value).catch(
           function (error) {
               var errorCode = error.code;
@@ -13,6 +15,26 @@ function passwordSignIn() {
                   alert(errorCode);
               }
       });
+    });
+  }
+  else {
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function() {
+      firebase.auth().signInWithEmailAndPassword(document.getElementById('email').value, document.getElementById('password').value).catch(
+          function (error) {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              if (errorCode == 'auth/wrong-password') {
+                  alert('Incorrect password')
+              }
+              else if (errorCode == 'auth/invalid-email') {
+                  alert('Email address is invalid')
+              }
+              else {
+                  alert(errorCode);
+              }
+      });
+    });
+  }
 }
 
 window.onload = function() {
