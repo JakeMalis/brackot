4cc059563e408d94b9d7a08ffec1295f1c9f405e
Jake Malis <jake@allstaresports.com>
2020-08-12 19:47:30 -0400

    Users now given Brackot Unlimited role on creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+
+exports.subscribeUserToUnlimited = functions.region('us-east1').auth.user().onCreate((user) => {
+  return admin.auth().setCustomUserClaims(user.uid, {subscription: "unlimited"});
 });
