e9900e218ede628286a7967f21df80080d836cdf
Jake Malis <jake@allstaresports.com>
2020-08-13 19:10:00 -0400

    Temporary Function returns amount of documents in mail collection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,19 @@ admin.initializeApp();
 exports.subscribeUserToUnlimited = functions.region('us-east1').auth.user().onCreate((user) => {
   return admin.auth().setCustomUserClaims(user.uid, {subscription: "unlimited"});
 });
+
+exports.deleteOldMail = functions.region('us-east1').firestore.document('mail/{documentId}').onCreate((snap, context) => {
+      //const newValue = snap.data();
+      //return newValue;
+      //var currentDate = new Date(newValue.startTime.toDate());
+      let count: number = 1;
+      async function removeMail() {
+        await admin.firestore().collection('mail').get().then(querySnapshot => {
+          querySnapshot.forEach(documentSnapshot => {
+            count++;
+          });
+        });
+        functions.logger.log("Hello from Firebase. Here's the number: ", count);
+      };
+      void removeMail();
+});
