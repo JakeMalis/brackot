f3f25dc5b65634505524f8ad25b460d46ffb58ff
JurassiCrafter <noobzdestiny@gmail.com>
2020-08-21 14:49:29 -0700

    Filter Buttons now Functional

--- public/scripts/toggle-filters.js
+++ public/scripts/toggle-filters.js
@@ -1,4 +1,15 @@
-function toggleFilterPopup() {
-  var popup = document.getElementById("filterPopup");
+function toggleGamePopup() {
+  var popup = document.getElementById("gamePopup");
   popup.classList.toggle("show");
+
+  var label = document.getElementById("gameButtonLabel");
+  label.classList.toggle("filterButtonLabelActive")
+}
+
+function toggleDatePopup() {
+  var popup = document.getElementById("datePopup");
+  popup.classList.toggle("show");
+
+  var label = document.getElementById("dateButtonLabel");
+  label.classList.toggle("filterButtonLabelActive")
 }
