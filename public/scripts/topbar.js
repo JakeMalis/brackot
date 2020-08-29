e501d3c5bf4d458978d68e512f0c07a73b78080c
JurassiCrafter <noobzdestiny@gmail.com>
2020-08-28 20:30:01 -0700

    Testy test test push

--- public/scripts/topbar.js
+++ public/scripts/topbar.js
@@ -1,15 +1,13 @@
 
 //I'm trying to figure out how to convert the header into a solid header after scrolling the page a little bit.
-/*
-$(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 50) {
-            $(".topNavbar").addClass("solidNavbar");
-        } else {
-           $(".topNavbar").removeClass("solidNavbar");
-        }
-    });
-});*/
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > 50) {
+        $("#topbar").addClass("solidNavbar");
+    }
+    else {
+        $("#topbar").removeClass("solidNavbar");
+    }
+});
 
 function toggleMenu(){
   var expandedTopbar = document.getElementById("topbarExpansion");
