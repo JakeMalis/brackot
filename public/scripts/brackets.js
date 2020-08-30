ed67c76d79bcdea6ee314d12aba513a137945b7a
Jake Malis <jake@allstaresports.com>
2020-08-30 02:01:52 -0400

    To be continued

--- public/scripts/brackets.js
+++ public/scripts/brackets.js
@@ -1,30 +1,100 @@
 var shuffledParticipants = [];
 var numParticipants = 0;
+<TournamentCard tournamentNumber={tournamentNumber} />
 
 class MatchCard extends React.Component {
   render() {
     return (
-        <div className={"match " + this.props.matchClass + " match" + this.props.roundNumber} id={"matchCard" + this.props.matchNumber}>
-          <div class={"participant " + this.props.half + "Half"}><img class="participantProfilePic" id={"participantLogo" + this.props.participantNumber}><p class="teamName" id={"participantName" + this.props.participantNumber}></p><p class="score whiteText" id={"participant" + this.props.participantNumber + "Match" + this.props.matchNumber + "Score"}></p></div>
-          <div class={"participant " + this.props.half + "Half"}><img class="profilePic" id={"participantLogo" + this.props.participantNumber}><p class="teamName" id={"participantName" + this.props.participantNumber}></p><p class="score whiteText" id={"participant" + this.props.participantNumber + "Match" + this.props.matchNumber + "Score"}></p></div>
-        </div>
+      <div className={"match " + "match" + this.props.roundNumber + this.props.empty} id={"matchCard" + this.props.matchNumber}>
+        <UpperParticipant participant={this.props.participants[0].uid} matchNumber={this.props.matchNumber} />
+        <LowerParticipant participant={this.props.participants[1].uid} matchNumber={this.props.matchNumber} />
+      </div>
     );
   }
 }
 
-async function renderMatchCards() {
-  var participantNumber = 1;
-  query.get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        TournamentCardArray.push(<TournamentCard tournamentNumber={doc.data().number} />);
-        participantNumber++
-    });
-  }).then(function() {
-    ReactDOM.render(
-      TournamentCardArray,
-      document.getElementById("row")
+class UpperParticipant extends React.Component {
+  render() {
+    return(
+      <React.Fragment>
+        <div className={"participant UpperHalf"}><img className="participantProfilePic" id={"participantLogo" + this.props.participant}></img><p className="teamName" id={"participantName" + this.props.participantNumber}></p><p className="score whiteText" id={"participant" + this.props.participant + "Match" + this.props.matchNumber + "Score"}></p></div>
+      </React.Fragment>
     );
-  });
+  }
+}
+
+
+class LowerParticipant extends React.Component {
+  render() {
+    return(
+      <React.Fragment>
+        <div className={"participant LowerHalf"}><img className="participantProfilePic" id={"participantLogo" + this.props.participant}></img><p className="teamName" id={"participantName" + this.props.participantNumber}></p><p className="score whiteText" id={"participant" + this.props.participant + "Match" + this.props.matchNumber + "Score"}></p></div>
+      </React.Fragment>
+    );
+  }
+}
+
+//match
+
+
+//variable names: roundNumber, matchNumber, participantNumber, half, empty
+
+//classNames = The rectangle containing two participants within it is called a match.  Every match has the class match.
+//              Depending on what round the match is in, it will have ANOTHER class, called match(insertRoundNumberHere).
+//              Finally, if the match is an empty space, and it is merely a space placeholder, and nobody will EVER players
+//              in that spot, then you add the class emptySpace as well.
+//EXAMPLES
+//match match2  -  a match in the second round with people playing in it
+//match match4  -  a match in the fourth round with people playing in it
+//match match1 emptySpace  -  an empty space of nothing at all used for spacing in the first round
+
+
+//              Now, for participants.  Each match has two participant elements within it.
+//              Participant elements have the class participant, and also have either the class upperHalf or lowerHalf
+//              Finally, there will be slots for participants that are TEMPORARILY empty, waiting for the next winner to
+//              fill that spot. In that case, the participant also has the class noParticipant, until someone fills it,
+//              at which point it is removed.
+//EXAMPLES
+//participant upperHalf  -  the first person in the index of a match, filling the "upper" spot in the match
+//participant lowerHalf  -  the second person in the index of a match, filling the "lower" spot in the match
+//participant upperHalf noParticipant  -  the upper half of a match doesn't have its player yet from the last match
+//                                         therefore, it has the noParticipant class to leave an empty spot in upperHalf
+
+async function renderMatchCards() {
+  var tournament = firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B");
+
+  for (var round = 1; round <= getByesAndRounds()[1]; round++){
+    var MatchColumnCards = []
+    var matchNumber = 1;
+    tournament.get().then(function(doc) {
+      var matchups = doc.data().("matchupsRound" + round);
+      matchups.forEach(function(entry) {
+        var upper = entry[0];
+        var lower = entry[1];
+        var upperParticipant, lowerParticipant;\
+        var participants = [];
+
+        if (entry === [[],[]]) { var empty = " emptySpace"; } else { var empty = ""; }
+        entry.forEach(function(childrenEntry, index)) {
+          if (childrenEntry === upper){
+            upperParticipant = { uid: childrenEntry };
+            participants.push(upperParticipant);
+          }
+          else {
+            lowerParticipant = { uid: childrenEntry };
+            participants.push(lowerParticipant);
+          }
+        });
+        MatchColumnCards.push(<MatchCard participantNumber={childrenEntry} roundNumber={round} matchNumber={matchNumber} empty={empty} participants={participants} />);
+        matchNumber++;
+      });
+    }).then(function() {
+      ReactDOM.render(
+        MatchColumnCards,
+        document.getElementById("matchColumn" + round)
+      );
+    });
+  }
 }
 
 
@@ -33,61 +103,80 @@ function personalizeElements() {
   firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").get().then(function(doc){
     shuffledParticipants = doc.data().players;
     numParticipants = doc.data().players.length;
-  });
+  }).then(function() {
+    console.log(shuffledParticipants);
+    shuffleParticipants();
 
-  shuffleParticipants();
-  createInitialMatches();
-  implementByes();
+    var byes = getByesAndRounds()[0];
+    var rounds = getByesAndRounds()[1];
+    var firstRound = createInitialMatches();
+    var secondRound = [];
 
-  var rounds = getByesAndRounds()[1];
-  if(rounds > 3){
-      for(int x = 0; x < rounds - 3; x++){
-        matches = nextRound(matches);   //add something to print or save each round
-      }
+    firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+      shuffledParticipants: shuffledParticipants,
+      matchupsRound1: firstround
+    }).then(function() {
+      console.log('Uploaded data')
+    });
+    if(byes > 0 & numParticipants > 2){
+      secondRound = implementByes();
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound2: secondRound
+      )};
+    }
+    else if(numParticipants > 2){
+      secondRound = nextRound(firstRound);
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound2: secondRound
+      )};
+    }
 
+    var matches = [];
+
+    if(rounds > 2){
+      matches = nextRound(secondRound);
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound3: matches
+      )}
+    }
+    if(rounds > 3){
+      matches = nextRound(matches);
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound4: matches
+    }
+    if(rounds > 4){
+      matches = nextRound(secondRound);
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound5: matches
+    }
+    if(rounds > 5){
+      matches = nextRound(matches);
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound6: matches
+    }
+    if(rounds > 6){
+      matches = nextRound(matches);
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound7: matches
+    }
+    if(rounds > 7){
+      matches = nextRound(secondRound);
+      firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
+        matchupsRound8: matches
+      });
+    }
   }
-
 }
 
-function findCenterY(element){
-  var div = $(element)[0].getBoundingClientRect();
-  var divY = div.top + div.height/2;
-  return divY;
-}
-
-function setConnectorHeight(parent1, parent2, connector){
-  document.getElementById(connector).style.height = heightBetween(parent1, parent2);
-}
-
-function heightBetween(element1, element2){
-  // find the vertical center of each element
-  var divY1 = findCenterY(element1);
-  var divY2 = findCenterY(element2);
-  // find the distance between the two vertical centers
-  var distance = (divY1 - divY2);
-  return distance;
-}
-
-
-/* shuffleParticipants */
 function shuffleParticipants(){
-  for(var i = doc.data().players.length - 1; i > 0; i--){
+  for(var i = numParticipants - 1; i > 0; i--){
     const j = Math.floor(Math.random() * i);
     const temp = shuffledParticipants[i];
     shuffledParticipants[i] = shuffledParticipants[j];
     shuffledParticipants[j] = temp;
   }
-
-//  shuffledParticipants.forEach(async function (element) {
-  //  console.log(element);
-    /*
-    firebase.firestore().collection("tournaments").doc("Sscjc6eqIdlQLMMZrD3B").update({
-        shuffledParticipants: shuffledParticipants;
-    });
-    */
-  };
-
 }
+
 
 function getByesAndRounds(){
   var byes = 0;
@@ -105,27 +194,27 @@ function getByesAndRounds(){
   return [byes, rounds];
   /* rounds returns number of rounds in the tournament */
   /* byes returns number of players that need byes in the first round */
-
+}
 
 function createInitialMatches(){
   var matches = [];
   var byes = getByesAndRounds()[0];
   var initialNumRounds = (numParticipants - byes)/2;
   if(byes >= initialNumRounds){
-    for(int x = 0; x < 2 * initialNumRounds; x+=2){
+    for(var x = 0; x < 2 * initialNumRounds; x+=2){
       matches.push([shuffledParticipants[x], shuffledParticipants[x+1]]);
       matches.push([[], []]);
     }
-    for(int y = 0; y < byes - initialNumRounds; y+=2){
+    for(var y = 0; y < byes - initialNumRounds; y+=2){
       matches.push([[],[]]);
     }
   }
   else{
-    for(int z = 0; z < 2 * byes; z+=2){
+    for(var z = 0; z < 2 * byes; z+=2){
       matches.push([shuffledParticipants[z], shuffledParticipants[z+1]]);
       matches.push([[], []]);
     }
-    for(int w = 0; w < 2 * (initialNumRounds - byes); w+=2){
+    for(var w = 0; w < 2 * (initialNumRounds - byes); w+=2){
       var indexStart = 2 * (byes + 1) - 1;
       matches.push([shuffledParticipants[indexStart + w], shuffledParticipants[indexStart + w+1]]);
     }
@@ -139,10 +228,9 @@ function assignWinner([playerOne, playerTwo]){
    if both press true send an error
    if neither has won return an empty slot [ , ]
   */
-
   var winner;
 
-  return winner;
+  return playerOne;
 }
 
 
@@ -153,12 +241,12 @@ function implementByes(){    /* creates second round of matches */
   var numOfWinners = (numParticipants-byes)/2;
   var count = 1;
 
-  for(int x = 0; x < 2*numOfWinners; x+=2){
-    matches.push([assignWinner(initialMatches()[count]), shuffledParticipants[shuffledParticipants.length - count]]);
+  for(var x = 0; x < 2*numOfWinners; x+=2){
+    matches.push([assignWinner(initialMatches[count]), shuffledParticipants[shuffledParticipants.length - count]]);
     count++;
   }
 
-  for(int y = 0; y < byes - numofWinners; y+=2){
+  for(var y = 0; y < byes - numofWinners; y+=2){
     matches.push([shuffledParticipants[2*numOfWinners + y], shuffledParticipants[2*numOfWinners + y + 1]]);
   }
 
@@ -168,8 +256,36 @@ function implementByes(){    /* creates second round of matches */
 
 function nextRound(lastRound){
   var matches = [];
-  for(int z = 0; z < lastRound.length; z+=2){
+  if(lastRound.length > 1){
+  for(var z = 0; z < lastRound.length; z+=2){
     matches.push([assignWinner(lastRound[z], lastRound[z+1])]);
-    return matches;
   }
+  return matches;
+}
+return assignWinner(lastRound[0]);
+}
+
+
+
+
+
+function openMatchModal() {
+  var modal = document.getElementById("matchModal");
+  modal.style.display = "block";
+}
+
+function closeMatchModal() {
+  var modal = document.getElementById("matchModal");
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function() {
+  modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      }
+    }
 }
