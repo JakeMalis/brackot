2efd2cc034047379f1011048bdf37c96241bb277
JurassiCrafter <noobzdestiny@gmail.com>
2020-09-01 23:00:13 -0700

    THE CAROUSEL FINALLY SCROLLS

---
+++ public/scripts/gamesCarousel.js
@@ -0,0 +1,9 @@
+function scrollCarouselRight(){
+  var carousel = document.getElementById("browseGamesCarousel");
+  carousel.scrollLeft += 387;
+}
+
+function scrollCarouselLeft(){
+  var carousel = document.getElementById("browseGamesCarousel");
+  carousel.scrollLeft -= 387;
+}
