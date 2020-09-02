fb364fb23962c790009489966bdeb8198def8a16
JurassiCrafter <noobzdestiny@gmail.com>
2020-09-01 23:38:44 -0700

    Scrolls on mobile too ;)

--- public/scripts/gamesCarousel.js
+++ public/scripts/gamesCarousel.js
@@ -1,9 +1,24 @@
+//This janky code doesn't work quite yet - to fix, first override the !important visibility on the arrows.
+
 function scrollCarouselRight(){
+  $('#leftArrow').removeClass("hiddenArrow");
   var carousel = document.getElementById("browseGamesCarousel");
   carousel.scrollLeft += 387;
+  var maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+
+  if(carousel.scrollLeft == maxScrollLeft){
+    $('#rightArrow').addClass("hiddenArrow");
+  }
 }
 
 function scrollCarouselLeft(){
+  $('#rightArrow').removeClass("hiddenArrow");
   var carousel = document.getElementById("browseGamesCarousel");
   carousel.scrollLeft -= 387;
+
+  var temp = carousel.scrollLeft;
+
+  if(temp == 0){
+    $('#leftArrow').addClass("hiddenArrow");
+  }
 }
