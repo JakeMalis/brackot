78ff1bd101a43bec6e4141a9c3f21461b2be202e
Jake Malis <jakebmalis@gmail.com>
2020-09-15 13:00:48 -0400

    Participants card does throw errors properly, will eventually fix others

--- public/scripts/participantCards.js
+++ public/scripts/participantCards.js
@@ -2,7 +2,7 @@ class ParticipantCard extends React.Component {
   render() {
     return(
       <div className={"participantCard"} id={"participantCard" + this.props.participantInfoNumber}>
-        <img className={"participantCardPic"} id={"participantCardPic" + this.props.participantInfoNumber} src="media/BrackotLogo2.jpg"></img>
+        <img className={"participantCardPic"} id={"participantCardPic" + this.props.participantInfoNumber}></img>
         <p className={"participantCardName"} id={"participantCardName" + this.props.participantInfoNumber}></p>
       </div>
     );
@@ -10,15 +10,15 @@ class ParticipantCard extends React.Component {
 }
 
 function renderParticipants(){
+  var ParticipantCards = [];
   firebase.firestore().collection("tournaments").doc(tournamentId).get().then(async function(doc) {
-      var ParticipantCards = [];
       var participantInfoNumber = 1;
 
       doc.data().players.forEach(function(participant) {
         ParticipantCards.push(<ParticipantCard participantInfoNumber={participantInfoNumber}/>);
         participantInfoNumber++;
       });
-
+    }).then(function() {
       ReactDOM.render(
         ParticipantCards,
         document.getElementById("participantsTab")
@@ -42,13 +42,12 @@ function loadParticipantData(){
 async function participantInfoLoadingFunction(entry, participantNumber) {
   firebase.firestore().collection("users").doc(entry).get().then(function(userDoc) {
     document.getElementById("participantCardName" + participantNumber).innerHTML = userDoc.data().name;
-    /*====================================GRAB USER PROFILE PICTURE========================================================== */
-      var gsReference = firebase.storage().refFromURL("gs://brackot-app.appspot.com/" + entry + "/profile");
-      gsReference.getDownloadURL().then(function (url) {
-        document.getElementById("participantCardPic" + participantNumber).src = url;
-      });
-  }).catch(function(error){
-    //Why isn't the below code working?  Great question....
-    console.log("No profile picture");
-  })
+    
+    var gsReference = firebase.storage().refFromURL("gs://brackot-app.appspot.com/" + entry + "/profile");
+    gsReference.getDownloadURL().then(function (url) {
+      document.getElementById("participantCardPic" + participantNumber).src = url;
+    }).catch(function(error) {
+      document.getElementById("participantCardPic" + participantNumber).src = "media/BrackotLogo2.jpg";
+    });
+  });
 }
