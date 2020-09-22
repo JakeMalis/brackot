ef37c1a91609804eb358992e3c81f31a0fab9e0e
Jake Malis <jakebmalis@gmail.com>
2020-09-22 09:35:51 -0400

    Started working on image compression functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,12 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as compress_images from 'compress-images';
 
 admin.initializeApp();
+
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
 exports.makeUserAdmin = functions.region('us-east1').https.onCall(async (data, context) => {
   if(!context.auth) throw new Error("User is not logged in");
@@ -34,3 +39,39 @@ exports.sendWelcomeEmail = functions.region('us-east1').auth.user().onCreate(asy
 
   functions.logger.log(link);
 });
+
+exports.compressProfilePicture = functions.region('us-east1').storage.object().onFinalize(async (object) => {
+  const fileType = object.contentType;
+  const filePath: string = object.name!;
+  const fileBucket = object.bucket;
+  const fileName = path.basename(filePath);
+
+  const bucket = admin.storage().bucket(fileBucket);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+
+  await bucket.file(filePath).download({destination: tempFilePath});
+  //functions.logger.log("Image downloaded locally to, ", tempFilePath);
+
+  if (fileType === "image/jpg") {
+    compress_images(
+      tempFilePath,
+      process.cwd(),
+      { compress_force: false, statistic: true, autoupdate: true },
+      false,
+      { jpg: { engine: "mozjpeg", command: ["-quality", "60"] }},
+      function (error, completed) {
+        if (completed === true) {
+          functions.logger.log("Compressed jpg file")
+        }
+      }
+    );
+  }
+
+  return fs.unlinkSync(tempFilePath);
+});
+
+/*
+exports.setUserProfilePicture = functions.region('us-east1').storage.object().onMetadataUpdate(async (object) => {
+
+});
+*/
