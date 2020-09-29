8f7bc01462e6b95196ffd0f6596cacc03cd1080a
Dimitri Pezaris <dimitripezaris@gmail.com>
2020-09-28 20:58:03 -0400

    chat send works now adding chat display

---
+++ public/scripts/chat.js
@@ -0,0 +1,71 @@
+const user = []
+async function updateMessage(msgObj) {
+    const db = firebase.firestore();
+    db.collection('tournaments').doc(tournamentId).collection('chat')
+        .add({
+            ...msgObj,
+            createdAt: new Date(),
+        })
+        .catch(error => {
+            console.log('error')
+        });
+    console.log('complete') 
+}
+async function getRealtimeConversations(user) {
+    
+
+        const db = firebase.firestore();
+        db.collection('conversations').doc(tournamentId).collection('chat')
+        .orderBy('createdAt', 'asc')
+        .onSnapshot((querySnapshot) => {
+
+            const convos = [];
+
+            querySnapshot.forEach(doc => {
+                convos.push(doc.data())
+            });
+            user.conversations = convos;
+            console.log(conversations);
+        })
+    
+}
+function initChat() {
+    getRealtimeConversations(firebase.auth().currentUser.uid);
+}
+function submitMessage() {
+    console.log('hello')
+    var message = document.getElementById("textHolder").value;
+    console.log(message)
+    const msgObj = {
+        sentUID: firebase.auth().currentUser.uid,
+        message
+    }
+    if(message !== ""){
+        console.log("into if statement")
+        updateMessage(msgObj)
+        
+    };
+    console.log("out of if")
+}
+function renderChat() {
+    ReactDOM.render(
+        Message,
+        document.getElementById("messageSections")
+    );
+}
+class Message extends React.Component {
+    render(){
+        return (
+            <div>
+                {
+                    user.conversations.map(con =>
+                        <div style={{ textAlign: con.userSent == firebase.auth().currentUser.uid
+                            ? 'right' : 'left' }}>
+                        <p className="messageStyle" >{con.message}</p>
+                        </div>)    
+                }
+            </div>
+            
+        );
+    }
+}
