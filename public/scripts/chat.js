c46fdb34fb79832b4dfdcd387d7fc25e8cd82aea
Dimitri Pezaris <dimitripezaris@gmail.com>
2020-09-29 17:42:23 -0400

    transfering computers

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,9 +1,35 @@
+import 
+
 const userConstants = {
     GET_REALTIME_USERS: 'GET_REALTIME_USERS',
     GET_REALTIME_MESSAGES: 'GET_REALTIME_MESSAGES'
 }
-const user = [
-]
+const user = useSelector(state => state.user);
+const intiState = {
+    users: [],
+    conversations: []
+}
+
+const updateState = (state = intiState, action) => {
+    console.log(action)
+    switch(action.type){
+        case userConstants.GET_REALTIME_MESSAGES:
+            state = {
+                ...state,
+                conversations: action.payload.conversations
+            }
+            break;
+        case `${userConstants.GET_REALTIME_MESSAGES}_FAILURE`:
+            state = {
+                ...state,conversations: []
+            }
+        break;
+    }
+
+
+    return state;
+
+}
 async function updateMessage(msgObj) {
     const db = firebase.firestore();
     db.collection('tournaments').doc(tournamentId).collection('chat')
@@ -16,22 +42,35 @@ async function updateMessage(msgObj) {
         });
     console.log('complete') 
 }
-async function getRealtimeConversations(user) {
+const getRealtimeConversations = (user) => {
+    
+        console.log("gotintorealtimeconvos")
         const db = firebase.firestore();
         db.collection('tournaments').doc(tournamentId).collection('chat')
         .orderBy('createdAt', 'asc')
         .onSnapshot((querySnapshot) => {
+
+            const convos = [];
+
             querySnapshot.forEach(doc => {
-                if(!user.includes(doc)) {
-                    user.push(doc)
-                }
+                conversations.push(doc.data())  
             });
-            
+            updateState({intiState,
+                type: 'GET_REALTIME_MESSAGES',
+                payload: {conversations}
+            })
+            state = {
+                ...state,
+                conversations: convos
+            }
+            console.log(conversations);
         })
     
 }
 function initChat() {
+    console.log("into init chat")
     getRealtimeConversations(firebase.auth().currentUser.uid);
+    renderChat()
 }
 function submitMessage() {
     console.log('hello')
